2a7624b6aca720227865cbc9f49820a599f8f7cd
jakeschultz89 <ejacobschultz@gmail.com>
2022-01-11 18:50:44 -0500

    collision fixed

---
+++ code/settings.py
@@ -0,0 +1,16 @@
+level_map = [
+'                                                        ',
+'                               XX                       ',
+'                                 XXXXXXXXXXX            ',
+' XX    XXX            XX       XX                       ',
+' XX P                                                   ',
+' XXXX         XX         XX       X     X     X  X XXXX ',
+' XXXX       XX                    X    X X    X X  XX   ',
+' XX    X  XXXX    XX  XX       X  X   XXXXX   XXX  X    ',
+'       X  XXXX    XX  XXX      XXXX  X     X  X  X XXXX ',
+'    XXXX  XXXXXX  XX  XXXX                              ',
+'XXXXXXXX  XXXXXX  XX  XXXX                              ']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
