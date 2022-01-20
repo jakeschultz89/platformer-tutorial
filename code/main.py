efe89d261af32c8648c7c332a7ed50af082548c9
jakeschultz89 <ejacobschultz@gmail.com>
2022-01-19 22:56:17 -0500

    starting new level design

--- code/main.py
+++ code/main.py
@@ -1,12 +1,13 @@
 import pygame, sys
-from settings import *
-from level import Level
+# from settings import *
+# from level import Level
 
 # Pygame setup
 pygame.init()
+screen_width = 1280
+screen_height = 720
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
-level = Level(level_map,screen)
 
 while True:
     for event in pygame.event.get():
@@ -15,7 +16,6 @@ while True:
             sys.exit()
 
     screen.fill("black")
-    level.run()
 
     pygame.display.update()
     clock.tick(60)
--- code/settings.py
+++ code/settings.py
@@ -1,15 +1,15 @@
 level_map = [
-'                                                        ',
-'                               XX                       ',
-'                                 XXXXXXXXXXX            ',
-' XX    XXX            XX       XX                       ',
-' XX P                                                   ',
-' XXXX         XX         XX       X     X     X  X XXXX ',
-' XXXX       XX                    X    X X    X X  XX   ',
-' XX    X  XXXX    XX  XX       X  X   XXXXX   XXX  X    ',
-'       X  XXXX    XX  XXX      XXXX  X     X  X  X XXXX ',
-'    XXXX  XXXXXX  XX  XXXX                              ',
-'XXXXXXXX  XXXXXX  XX  XXXX                              ']
+'                           ',
+'                           ',
+'                           ',
+' XX    XXX            XX   ',
+' XX P                      ',
+' XXXX         XX         XX',
+' XXXX       XX             ',
+' XX    X  XXXX    XX  XX   ',
+'       X  XXXX    XX  XXX  ',
+'    XXXX  XXXXXX  XX  XXXX ',
+'XXXXXXXX  XXXXXX  XX  XXXX ']
 
 tile_size = 64
 screen_width = 1200
